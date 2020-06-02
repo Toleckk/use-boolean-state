252376ca9c52ddf4ece8515f6c068da61262f656
Toleckk <anatolich1995@gmail.com>
2020-06-02 18:15:32 +0300

    useBooleanState tests added

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,29 +1,62 @@
-import { useMyHook } from './'
-import { renderHook, act } from "@testing-library/react-hooks";
+import {useBooleanState} from './'
+import {renderHook, act} from "@testing-library/react-hooks"
 
-// mock timer using jest
-jest.useFakeTimers();
 
-describe('useMyHook', () => {
-  it('updates every second', () => {
-    const { result } = renderHook(() => useMyHook());
+jest.useFakeTimers()
 
-    expect(result.current).toBe(0);
+describe.each([true, false])(
+  'useBooleanState',
+  (initialValue: Boolean) => {
+    it('should keep default value', () => {
+      const {result} = renderHook(() => useBooleanState(initialValue))
 
-    // Fast-forward 1sec
-    act(() => {
-      jest.advanceTimersByTime(1000);
-    });
+      expect(result.current[0]).toBe(initialValue)
+    })
 
-    // Check after total 1 sec
-    expect(result.current).toBe(1);
 
-    // Fast-forward 1 more sec
-    act(() => {
-      jest.advanceTimersByTime(1000);
-    });
+    it('should turn state to true by second tuple element', () => {
+      const {result} = renderHook(() => useBooleanState(initialValue))
 
-    // Check after total 2 sec
-    expect(result.current).toBe(2);
+      const setTrue = result.current[1]
+
+      act(() => setTrue())
+
+      expect(result.current[0]).toBe(true)
+    })
+
+
+    it('should turn state to false by third tuple element', () => {
+      const {result} = renderHook(() => useBooleanState(initialValue))
+
+      const setFalse = result.current[2]
+
+      act(() => setFalse())
+
+      expect(result.current[0]).toBe(false)
+    })
+
+
+    it('should invert state by fourth tuple element', () => {
+      const {result} = renderHook(() => useBooleanState(initialValue))
+
+      const toggle = result.current[3]
+
+      act(() => toggle())
+
+      expect(result.current[0]).toBe(!initialValue)
+    })
+
+
+    test.each([true, false])(
+      'should set value by fifth element',
+      (futureValue: Boolean) => {
+        const {result} = renderHook(() => useBooleanState(initialValue))
+
+        const setState = result.current[4]
+
+        act(() => setState(futureValue))
+
+        expect(result.current[0]).toBe(futureValue)
+      }
+    )
   })
-})
